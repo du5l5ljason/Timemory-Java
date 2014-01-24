77223905e97023c4384c027fa95c4993c0fc979d
Tingfang <tfang.du@gmail.com>
2014-01-23 17:22:23 -0800

    Code style cleanup.

--- src/data/WorldCoords.java
+++ src/data/WorldCoords.java
@@ -1,108 +1,108 @@
-/* 
+/*
  * Class: WorldCoords.java
  * Description: describe any location in the world as a tuple of (latitude, longitude, altitude).
  */
 package data;
 import utils.GeoMathUtils;
 
-public class WorldCoords{ 
-	
-	final double lat, lon, alt;
-	
-	/**
-	 * Construct the WorldCoords object. (double values)
-	 */
-	public WorldCoords(double lat, double lon, double alt){
-		this.lat = lat;
-		this.lon = lon;
-		this.alt = alt;
-	}
-	
-	/**
-	 * Construct the WorldCoords object (int values)
-	 */
-	
-	public WorldCoords(int lat, int lon, int alt){
-		this.lat = lat;
-		this.lon = lon;
-		this.alt = alt;
-	}
-	
-	/**
-	 * Construct the WorldCoords object from another object.
-	 */
-	public WorldCoords(WorldCoords loc){
-		this.lat = loc.lat;
-		this.lon = loc.lon;
-		this.alt = loc.alt;
-	}
-	
-	/**
-	 * Construct the WorldCoords object by default.
-	 */
-	public WorldCoords(){
-		this.lat = 0;
-		this.lon = 0;
-		this.alt = 0;
-	}
-	
-	/**
-	 * Getters
-	 */
-	/*
-	 * Get lat
-	 */
-	public double Lat(){
-		return lat;
-	}
-	
-	/*
-	 * Get log
-	 */
-	public double Lon(){
-		return lon;
-	}
-	
-	/*
-	 * Get alt
-	 */
-	public double Alt(){
-		return alt;
-	}
+public class WorldCoords{
 
-	/**
-	 * Setters
-	 */
-	/*
-	 * Set lat
-	 */
-	public WorldCoords setLat(double lat){
-		return new WorldCoords(lat, lon, alt);
-	}
-	
-	/*
-	 * Set log
-	 */
-	public WorldCoords setLog(double lon){
-		return new WorldCoords(lat, lon, alt);
-	}
-		
-	/*
-	 * Set alt
-	 */
-	public WorldCoords setAlt(double alt){
-		return new WorldCoords(lat, lon, alt);
-	}
-		
-	/*
-	 * Get the distance away from a point.
-	 */
-	public double distanceInKm(WorldCoords loc){
-		return GeoMathUtils.distInKmBetween(this, loc);
-	}
-	
-	public double distanceInMiles(WorldCoords loc){
-		return GeoMathUtils.distInMilesBetween(this, loc);
-	}
-	
+    final double lat, lon, alt;
+
+    /**
+     * Construct the WorldCoords object. (double values)
+     */
+    public WorldCoords(double lat, double lon, double alt){
+        this.lat = lat;
+        this.lon = lon;
+        this.alt = alt;
+    }
+
+    /**
+     * Construct the WorldCoords object (int values)
+     */
+
+    public WorldCoords(int lat, int lon, int alt){
+        this.lat = lat;
+        this.lon = lon;
+        this.alt = alt;
+    }
+
+    /**
+     * Construct the WorldCoords object from another object.
+     */
+    public WorldCoords(WorldCoords loc){
+        this.lat = loc.lat;
+        this.lon = loc.lon;
+        this.alt = loc.alt;
+    }
+
+    /**
+     * Construct the WorldCoords object by default.
+     */
+    public WorldCoords(){
+        this.lat = 0;
+        this.lon = 0;
+        this.alt = 0;
+    }
+
+    /**
+     * Getters
+     */
+    /*
+     * Get lat
+     */
+    public double Lat(){
+        return lat;
+    }
+
+    /*
+     * Get log
+     */
+    public double Lon(){
+        return lon;
+    }
+
+    /*
+     * Get alt
+     */
+    public double Alt(){
+        return alt;
+    }
+
+    /**
+     * Setters
+     */
+    /*
+     * Set lat
+     */
+    public WorldCoords setLat(double lat){
+        return new WorldCoords(lat, lon, alt);
+    }
+
+    /*
+     * Set log
+     */
+    public WorldCoords setLog(double lon){
+        return new WorldCoords(lat, lon, alt);
+    }
+
+    /*
+     * Set alt
+     */
+    public WorldCoords setAlt(double alt){
+        return new WorldCoords(lat, lon, alt);
+    }
+
+    /*
+     * Get the distance away from a point.
+     */
+    public double distanceInKm(WorldCoords loc){
+        return GeoMathUtils.distInKmBetween(this, loc);
+    }
+
+    public double distanceInMiles(WorldCoords loc){
+        return GeoMathUtils.distInMilesBetween(this, loc);
+    }
+
 }
--- src/utils/GeoMathUtils.java
+++ src/utils/GeoMathUtils.java
@@ -1,27 +1,27 @@
-/* 
+/*
  * Class: GeoMathUtils.java
  * Description: Provide math utils such as calculating distance between two points in Geo coordinate system.
  */
- 
+
  package utils;
  import data.WorldCoords;
- 
+
  public final class GeoMathUtils{
 
     public static final double EARTH_RADIUS_IN_KM = 6367;
     public static final double EARTH_RADIUS_IN_MILES = 3956;
-    
+
     public static double distInKmBetween(WorldCoords pos1, WorldCoords pos2){
-        
+
         return distBetween(pos1, pos2, EARTH_RADIUS_IN_KM);
     }
-    
+
     public static double distInMilesBetween(WorldCoords pos1, WorldCoords pos2){
-    
+
     return distBetween(pos1, pos2, EARTH_RADIUS_IN_MILES);
     }
     public static double distBetween(WorldCoords pos1, WorldCoords pos2, double scale){
-        
+
         /*
         double dLat = radians(pos2.Lat() - pos1.Lat());
         double dLon = radians(pos2.Lon() - pos1.Lon());
@@ -29,5 +29,4 @@
         return 0;
     }
  }
- 
- 
+
